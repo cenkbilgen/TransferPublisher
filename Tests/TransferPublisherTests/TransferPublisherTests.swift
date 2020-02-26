8191daffd5a0c668a82c88c18b6be09de04f7b4f
cenkbilgen <cenk.bilgen@gmail.com>
2020-02-26 12:38:09 -0500

    Add a simple expected usage test case

--- Tests/TransferPublisherTests/TransferPublisherTests.swift
+++ Tests/TransferPublisherTests/TransferPublisherTests.swift
@@ -1,17 +1,40 @@
 import XCTest
 @testable import TransferPublisher
+import Combine
+#if os(iOS)  // TODO: Just need to include AppKit for the macOS test
+import UIKit
+
 
 final class TransferPublisherTests: XCTestCase {
+  
+  var cancellables = Set<AnyCancellable>()
+  
+  // Just a simple test to download and see progress in console
   func testDownload() {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct
-    // results.
     
-    let session = TransferSession()
+    let waiter = XCTWaiter()
+    let networkResponded = XCTestExpectation()
+    
+    let request = URLRequest(url: URL(string: "https://upload.wikimedia.org/wikipedia/commons/8/89/Wilkin_River_close_to_its_confluence_with_Makarora_River%2C_Otago%2C_New_Zealand.jpg")!)
+    
+    URLSession(configuration: .default).downloadTaskPublisher(with: request)
+      .sink(receiveCompletion: { (completion) in
+        print(completion)
+        networkResponded.fulfill()
+      }) { (output) in
+        print(output)
+        if case .complete(let data) = output {
+          print("Received file: \(data.count)")
+          let image = UIImage(data: data)
+          print(image)
+          
+        }
+    }.store(in: &cancellables)
+    
+    waiter.wait(for: [networkResponded], timeout: 30)
     
   }
   
-  static var allTests = [
-    ("testExample", testExample),
-  ]
 }
+
+#endif
